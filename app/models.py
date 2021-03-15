31c94ee71a2f7abb72d287cc8420f141f667b731
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-03-15 14:30:09 +0300

    dropped db and recreated to allow text

--- app/models.py
+++ app/models.py
@@ -1,106 +1,74 @@
-from . import db
+from . import db, login_manager
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from . import login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+from time import time
+from app import create_app
 
 @login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(UserMixin,db.Model):
-
-    'User model schema'
-
+def load_user(id):
+        return User.query.get(int(id))
+        
+class User(UserMixin, db.Model):
+    '''
+    UserMixin class that includes generic implementations
+    that are appropriate for most user model classes
+    '''
     __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True,index = True)
-    bio = db.Column(db.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(130))
     profile_pic_path = db.Column(db.String())
-    pass_secure = db.Column(db.String(255))
-    articles = db.relationship('Article',backref = 'user',lazy = "dynamic")
-    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
-    
-    
+
+    post = db.relationship('Post', backref='user', lazy="dynamic")
+    comments = db.relationship('Comments', backref='user', lazy="dynamic")
+
+    pass_secure  = db.Column(db.String(255))
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
+   
+    def __repr__(self):
+        return '{}'.format(self.username)
+    
+class Post(db.Model):
+    '''
+    Post class represent the posts posted by 
+    users.
+    ''' 
+
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    comments = db.relationship('Comments', backref='user', lazy="dynamic")
+
+    @classmethod
+    def retrieve_posts(cls, id):
+        posts = Post.filter_by(id=id).all()
+        return posts
 
 
     def __repr__(self):
-        return f'User {self.username}'
+        return '{}'.format(self.body)
 
 
-
-class Quote:
-    '''
-    Quote class to define Quote Objects
-    '''
-
-    def __init__(self,id,quote_text,quote_author):
-        self.id =id
-        self.quote_text = quote_text
-        self.quote_author = quote_author   
-
-
-
-class Article(db.Model):
-
-    'Article model schema'
-    
-    __tablename__ = 'articles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    article_title = db.Column(db.String)
-    article_body = db.Column(db.String)
-    article_tag = db.Column(db.String)
-    article_cover_path = db.Column(db.String())
-    article_comments_count = db.Column(db.Integer, default=0)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    article_upvotes = db.Column(db.Integer, default=0)
-    article_downvotes = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-    comments = db.relationship('Comment',backref = 'article',lazy = "dynamic")
-
-    def save_article(self):
-        db.session.add(self)
-        db.session.commit()
-
-
-    @classmethod
-    def get_all_articles(cls):
-        articles = Article.query.order_by(Article.posted.desc()).all()
-        return articles
-
-    @classmethod
-    def get_user_articles(cls,id):
-        articles = Article.query.filter_by(user_id=id).order_by(Article.posted.desc()).all()
-        return articles          
-
-
-
-class Comment(db.Model):
-
-    'Comment model schema'
-    
+class Comments(db.Model):
     __tablename__ = 'comments'
-
-    id = db.Column(db.Integer,primary_key = True)
-    comment = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-    article_id = db.Column(db.Integer,db.ForeignKey("articles.id"))
-
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(255))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
