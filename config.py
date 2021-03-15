47f66716d27f8489f9007513fd5b53c13437cc88
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-03-15 13:58:22 +0300

    added markdown

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
+
+
 class Config:
+
     '''
     General configuration parent class
     '''
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-
+    
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -14,35 +20,29 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
 class ProdConfig(Config):
+
     '''
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Moringa@localhost/blog_test'
-
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    
 
 class DevConfig(Config):
+
     '''
     Development  configuration child class
-
     Args:
-      Config: The parent configuration class with General configuration settings
+        Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Moringa@localhost/blog_'
+    
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
+    'development': DevConfig,
+    'production': ProdConfig
 }
